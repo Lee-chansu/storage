32f0e470eaa918e6154b50b4521389fe7849f5e5
Lee-Chansu <tutle482@gmail.com>
2024-01-08 17:19:30 +0900

    중간저장

--- javascript/js-basic_1-ex-1.js
+++ javascript/js-basic_1-ex-1.js
@@ -6,17 +6,17 @@ for(let i = 0; i < menu.length; i++) {
 
         let panel = this.nextElementSibling;
 
-        menuClose(menu); // 전체 패널 닫기
+        menuClose(i, menu); // 전체 패널 닫기
         menuOpen(menu[i], panel); // 클릭한 패널 열기
-        
-
     });
 }
 
-function menuClose(item){
+function menuClose(i, item){
     for(let j = 0; j < item.length; j++){
-        item[j].nextElementSibling.style.maxHeight = null;
-        item[j].classList.remove('on');
+        if(i != j){// i (=현재 클릭한 요소의 순번) 가 아닌 것은 아래의 효과 적용
+            item[j].nextElementSibling.style.maxHeight = null;
+            item[j].classList.remove('on');
+        }
     }
 }
 
@@ -28,7 +28,7 @@ function menuOpen(menu, panel){
         menu.classList.remove('on');
     }
     else{
-        panel.style.maxHeight = item.scrollHeight + "px";
+        panel.style.maxHeight = panel.scrollHeight + "px";
         menu.classList.add('on');
     }
     
--- javascript/js-layout.js
+++ javascript/js-layout.js
@@ -1,11 +1,23 @@
 let question = document.getElementsByClassName('question');
+let after = document.get
 
-for(let i = 0; i < question.length; i++){
-    question[i].addEventListener('click', function(){
-        this.classList.toggle('on');
-
+for(let i of question){
+    i.addEventListener('click', function(){
         let answer = this.nextElementSibling;
 
+        for(let j of question){
+            if(i != j){
+                j.nextElementSibling.style.maxHeight = null;
+            }
+        }
 
-    })
+        if(answer.style.maxHeight){
+            answer.style.maxHeight = null;
+        }
+        else{
+            
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        }
+
+    });
 }
