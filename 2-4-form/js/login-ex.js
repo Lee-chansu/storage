30569256fc65e8aa2638a9af03e221f89ddc489d
Lee-Chansu <tutle482@gmail.com>
2024-01-17 17:01:17 +0900

    쉬는시간

--- 2-4-form/js/login-ex.js
+++ 2-4-form/js/login-ex.js
@@ -10,6 +10,7 @@ let userId = document.getElementById("id");
 let userPw = document.getElementById("pw");
 let loginBtn = document.getElementById("loginBtn");
 
+let errorBox = document.querySelector(".errorBox");
 let errorMessage = document.querySelector(".errorMessage");
 let regex = /(?=.*[a-z])(?=.*[\d])(?=.*[@#$%^&*!?])[a-z\d@#$%^&*!?]+/
 let regex2 = /(?=.*[a-z])+(?=.*[\d])+(?=.*[!@#$%^&*()?])/
@@ -37,19 +38,32 @@ loginBtn.addEventListener('click', function(){
   let pwValue= userPw.value;
 
   if(idValue == "" && pwValue == ""){
-    errorMessage.innerHTML("아이디와 비밀번호를 입력하세요")
+    errorMessage.innerHTML="아이디와 비밀번호를 입력하세요"
+    errorBox.classList.remove('blind')
   }
   else if(idValue == ""){
-    errorMessage.innerHTML("아이디를 입력하세요")
+    errorMessage.innerHTML="아이디를 입력하세요"
+    errorBox.classList.remove('blind')
   }
   else if(pwValue == ""){
-    errorMessage.innerHTML("비밀번호를 입력하세요")
+    errorMessage.innerHTML="비밀번호를 입력하세요"
+    errorBox.classList.remove('blind')
   }//4~12
   else if(idValue.length < 4 || idValue.length > 12){
-    errorMessage.innerHTML("아이디는 4~12글자입니다.")
+    errorMessage.innerHTML="아이디는 4~12글자입니다."
+    errorBox.classList.remove('blind')
   }//소문자, 숫자, 특수문자 1개 이상씩
-  else if(pwValue != regex2){
-    errorMessage.innerHTML("비밀번호 형식이 올바르지 않습니다.")
+  else if(pwValue != regex){
+    errorMessage.innerHTML="비밀번호 형식이 올바르지 않습니다."
+    errorBox.classList.remove('blind')
+  }
+  else if(idValue == userInfo.id && pwValue == userInfo.pw){
+    alert("로그인 성공");
+    return;
+  }
+  else{
+    alert("알 수없는 오류로 인해 로그인이 되지 않습니다. 지속적인 문제가 발생하는 경우 관리자에게 문의해주시기 바랍니다.");
+    return;
   }
 
 
