0f856a079d332e58948311cc68aa89ad45625f2d
Lee-Chansu <tutle482@gmail.com>
2024-02-01 10:10:41 +0900

    2.1 js 변경 실패

--- knotted_donut/knotted_donut-js/knotted-donut.js
+++ knotted_donut/knotted_donut-js/knotted-donut.js
@@ -5,11 +5,9 @@ for(let i of item){
     i.addEventListener('click', function(){
         let list2 = i.nextElementSibling;
         let scrollheight = list2.scrollHeight+'px';
-        let it_height = getComputedStyle(list2).maxHeight;
         
         
         if(list2.style.maxHeight == 0+'px'){
-            i.style.maxHeight = 0+'px';
             list2.style.maxHeight = scrollheight;
         }
         else{
