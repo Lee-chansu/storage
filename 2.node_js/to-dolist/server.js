85e3b7281a2daaf682e9c24e3bcdb0a7f832fd91
Lee-Chansu <tutle482@gmail.com>
2024-02-19 17:13:57 +0900

    개프-중간저장 중

--- 2.node_js/to-dolist/server.js
+++ 2.node_js/to-dolist/server.js
@@ -25,11 +25,23 @@ app.listen(3000 , ()=>{
 
 // 4. 하위페이지들 - 라우팅 
 
-app.get('/', (req, res)=>{
+//api
+app.get('/', async(req,res)=>{
   res.render('index.ejs')
-})
+}); // index.ejs 보여주기
 
-app.get('/login', (req, res)=>{
+app.get('/login', async(req,res)=>{
   res.render('login.ejs')
+});  //login.ejs 보여주기
 
-})
+
+app.get('/regist', ); //regist.ejs 보여주기
+app.post('/join', ); //-회원가입 처리
+
+
+app.post('/regist') // to-dolist 등록하기
+
+app.put('/set', );
+app.delete('/del/:username', ); // 회원탈퇴
+app.delete('/del/:id')
+
