8aab491b4d335eac52c3bf1ffb95bd42635024c2
Lee-chansu <tutle482@gmail.com>
2024-02-27 17:46:17 +0900

    02.27 react 복습 및 객체 만들고 활용하기

--- 3.react/02.27/post/src/components/Box.js
+++ 3.react/02.27/post/src/components/Box.js
@@ -3,6 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 /*eslint-disabled*/
 function Box(props) {
   let {i, post, setPost, setPick} = props;
+  
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+
   return(
     <>
       <div className='col wrap' onClick={()=>{ setPick(i) }}>
@@ -10,7 +19,7 @@ function Box(props) {
           <p className='col-1'>{post.id}</p>
           <h3 className='col-8'>{post.title}</h3>
           <p className='col-1'>{post.rating}</p>
-          <p classNmae='col-2'>{post.createdAt}</p>
+          <p classNmae='col-2'>{formatDate(post.createdAt)}</p>
         </div>
       </div>
     </>
--- 3.react/02.27/post/src/components/Detail.js
+++ 3.react/02.27/post/src/components/Detail.js
@@ -3,12 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 /*eslint-disabled*/
 function Detail(props){
   let {i, post} = props;
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+
   return(
     <div className='detail'>
       <div className='inner-box'>
         <h3>제목: {post[i].title} </h3>
         <p>상세 : {post[i].content} </p>
-        <p>출간일 : {post[i].createdAt}</p>
+        <p>출간일 : {formatDate(post[i].createdAt)}</p>
       </div>
     </div>
   )
