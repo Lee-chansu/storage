45ac30b6c97140d44d9c22dd42ea5275f68261f7
Lee-chansu <tutle482@gmail.com>
2024-03-13 16:23:53 +0900

    setting radio버튼으로 변경

--- 4.Next/03.13/mall/pages/setting.js
+++ 4.Next/03.13/mall/pages/setting.js
@@ -6,8 +6,8 @@ export default function Setting() {
   const { theme, setTheme } = useTheme();
   //useTheme안에 있는 ThemeContext에서 정의된 theme를 불러옴.
 
-  const handleChange = () => {
-    setTheme();
+  const handleChange = (e) => {
+    setTheme(e.target.value);
   };
 
   return (
@@ -15,9 +15,9 @@ export default function Setting() {
       <h2 className={styles.title}>설정</h2>
       <section className={styles.section}>
         <h3 className={styles.sectionTitle}>테마 설정</h3>
-        <input type="radio" name="theme" value="light" onClick={() => {}} />
+        <input type="radio" name="theme" value="light" onClick={handleChange} />
         light
-        <input type="radio" name="theme" value="dark" onClick={() => {}} />
+        <input type="radio" name="theme" value="dark" onClick={handleChange} />
         dark
       </section>
     </div>
